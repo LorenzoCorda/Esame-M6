86c4f1727d01645465741b1c8c2772d611b45374
Lorenzo <cordalorenzowork@gmail.com>
2025-06-20 17:56:24 +0200

    add something

--- Frontend/src/components/contexts/PostsContext.jsx
+++ Frontend/src/components/contexts/PostsContext.jsx
@@ -7,24 +7,6 @@ export const PostsProvider = ({ children }) => {
   const [isPostsLoading, setIsPostsLoading] = useState(true);
   const [error, setError] = useState("");
 
-  /* const getAllPosts = async () => {
-    try {
-      const response = await fetch("http://localhost:9099/posts", {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2YwZjg5ZjI5ODZmNDAwMTVkYTNhOGQiLCJpYXQiOjE3NDcxNzAzMjQsImV4cCI6MTc0ODM3OTkyNH0.15I8cd0VMZIYcbe7o5PdflpMwvDbqVHfpJeyGWGVRi0",
-        },
-      });
-      const posts = await response.json();
-      setPosts(posts);
-      setIsPostsLoading(false);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsPostsLoading(false);
-    }
-  }; */
-
   const getPosts = async () => {
     try {
       setIsPostsLoading(true);
